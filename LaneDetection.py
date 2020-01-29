218a8cb285a857975a026da0822b6249ff3b84e8
murtazahassan <mhpr@live.co.uk>
2020-01-29 20:12:01 +0300

    Update LaneDetection.py

--- LaneDetection.py
+++ LaneDetection.py
@@ -1,12 +1,11 @@
 import numpy as np
 import cv2
-from LaneDetectionA import utlis
+from CurvedLaneDetection import utlis
 
-
-####################################################
 ####################################################
 
 cameraFeed= False
+videoPath = 'project_video.mp4'
 cameraNo= 1
 frameWidth= 640
 frameHeight = 480
@@ -15,7 +14,6 @@ if cameraFeed:intialTracbarVals = [24,55,12,100] #  #wT,hT,wB,hB
 else:intialTracbarVals = [42,63,14,87]   #wT,hT,wB,hB
 
 ####################################################
-####################################################
 
 
 if cameraFeed:
@@ -23,22 +21,20 @@ if cameraFeed:
     cap.set(3, frameWidth)
     cap.set(4, frameHeight)
 else:
-    cap = cv2.VideoCapture('project_video.mp4')
-
-
+    cap = cv2.VideoCapture(videoPath)
 count=0
 noOfArrayValues =10
 global arrayCurve, arrayCounter
 arrayCounter=0
 arrayCurve = np.zeros([noOfArrayValues])
 myVals=[]
-
 utlis.initializeTrackbars(intialTracbarVals)
+
 
 while True:
 
-    #success, img = cap.read()
-    img = cv2.imread('test3.jpg')
+    success, img = cap.read()
+    #img = cv2.imread('test3.jpg')
     if cameraFeed== False:img = cv2.resize(img, (frameWidth, frameHeight), None)
     imgWarpPoints = img.copy()
     imgFinal = img.copy()
@@ -56,8 +52,8 @@ while True:
         lane_curve = np.mean([curverad[0], curverad[1]])
         imgFinal = utlis.draw_lanes(img, curves[0], curves[1],frameWidth,frameHeight,src=src)
 
-        ## Average
-        currentCurve = lane_curve // 10
+        # ## Average
+        currentCurve = lane_curve // 50
         if  int(np.sum(arrayCurve)) == 0:averageCurve = currentCurve
         else:
             averageCurve = np.sum(arrayCurve) // arrayCurve.shape[0]
@@ -65,19 +61,24 @@ while True:
         else :arrayCurve[arrayCounter] = currentCurve
         arrayCounter +=1
         if arrayCounter >=noOfArrayValues : arrayCounter=0
-        cv2.putText(imgFinal, str(int(averageCurve)), (frameWidth//2-30, 70), cv2.FONT_HERSHEY_DUPLEX, 1.75, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(imgFinal, str(int(averageCurve)), (frameWidth//2-70, 70), cv2.FONT_HERSHEY_DUPLEX, 1.75, (0, 0, 255), 2, cv2.LINE_AA)
 
     except:
         lane_curve=00
         pass
 
+    imgFinal= utlis.drawLines(imgFinal,lane_curve)
+
+
     imgThres = cv2.cvtColor(imgThres,cv2.COLOR_GRAY2BGR)
     imgBlank = np.zeros_like(img)
-    imgStacked = utlis.stackImages(0.4, ([img,imgUndis,imgWarpPoints],
+    imgStacked = utlis.stackImages(0.7, ([img,imgUndis,imgWarpPoints],
                                          [imgColor, imgCanny, imgThres],
                                          [imgWarp,imgSliding,imgFinal]
                                          ))
+
     cv2.imshow("PipeLine",imgStacked)
+    cv2.imshow("Result", imgFinal)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
